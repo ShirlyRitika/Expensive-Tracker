e2a9f1ee6a202f38aabfb3c7fad4901c0302f030
ShirlyRitika <151626895+ShirlyRitika@users.noreply.github.com>
2025-08-15 12:49:51 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ExpensiveTracker from './ExpensiveTracker'
+const App = () => {
+  return (
+    <div>
+      <ExpensiveTracker/>
+    </div>
+  )
+}
+export default App
